4b682e9d5441c8ec6bb73f3cbb8b7d0103df974f
Mudza09 <edunet09@gmail.com>
2020-06-01 21:07:31 +0700

    add blockit syntax higlighter

---
+++ src/assets/js/vendor/blockit-syntax.js
@@ -0,0 +1,12 @@
+/*! blockit-syntax.js | https://www.indonez.com | Indonez | MIT License */
+let sectionName = location.pathname.replace(/\.[^\.\/]+$/, "").substr(1)
+let sectionArray = Array.from(document.getElementsByClassName('uk-section')).splice(2)
+sectionArray.splice(sectionArray.length - 1)
+
+for (let i = 0; i < sectionArray.length; i++) {
+    let blockitSyntax = `
+    <div class="uk-flex uk-flex-right in-blockit-syntax">
+        <pre><code class="language-html">{{> section-${sectionName}-${i+1} }}</code></pre>
+    </div>`
+    sectionArray[i].children[0].insertAdjacentHTML("afterBegin", blockitSyntax)
+}
