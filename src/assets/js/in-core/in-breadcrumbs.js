e630011e81ac3b9b2ff4f8955319a396cd74efc2
Mudza09 <edunet09@gmail.com>
2020-06-09 12:13:51 +0700

    refactoring in-breadcrumb.js and fix github button offset the nav wrapper

--- src/assets/js/in-core/in-breadcrumbs.js
+++ src/assets/js/in-core/in-breadcrumbs.js
@@ -1,36 +1,22 @@
 /* in-breadcrumb.js | https://www.indonez.com | Indonez | MIT License */
 (function() {
-    const navbar = document.querySelectorAll('.uk-navbar-nav li');
-    const breadWrap = document.getElementsByClassName('uk-breadcrumb');
-    let breadChild;
+    const homeTitle = 'Home';
+    const homePath = document.querySelectorAll('.uk-navbar-nav li')[0].getElementsByTagName('a')[0].pathname;
+    const currentActive = document.querySelectorAll('.uk-navbar-nav li.uk-active');
+    const breadWrapper = document.getElementsByClassName('uk-breadcrumb');
+    let breadTitle;
 
-    // Check if breadcrumb is present
-    if (breadWrap.length > 0) {
-        const homePath = navbar[0].getElementsByTagName('a')[0].pathname;
-        const homeTitle = "Home";
+    if (breadWrapper.length > 0) {
+        breadWrapper[0].innerHTML = `<li><a href="${homePath.slice(location.pathname.lastIndexOf('/') + 1)}">${homeTitle}</a></li>`;
 
-        breadWrap[0].innerHTML = `<li><a href="${homePath.slice(location.pathname.lastIndexOf('/') + 1)}">${homeTitle}</a></li>`;
+        currentActive.forEach(function(el) {
+            const createLi = document.createElement('li');
+            breadTitle = el.childNodes[0].innerText;
 
-        navbar.forEach(function(el) {
-            if (el.getAttribute('class', 'uk-active') === 'uk-active') {
-                const createLi = document.createElement('li');
-                const hrefLi = el.getElementsByTagName('a');
-                const breadPath = hrefLi[0].pathname;
-                breadChild = el.childNodes[0].innerText;
-
-                if (hrefLi[0].pathname === hrefLi[0].pathname) {
-                    createLi.innerHTML = `<a href="${hrefLi[0].href[hrefLi[0].href.length - 1]}">${breadChild}</a>`;
-                } else if (hrefLi[0].href[hrefLi[0].href.length - 1] !== '#') {
-                    createLi.innerHTML = `<a href="${breadPath.slice(location.pathname.lastIndexOf('/') + 1)}">${breadChild}</a>`;
-                }
-
-                breadWrap[0].insertBefore(createLi, breadWrap[0].firstElementChild.previousSibling);
-            }
-        });
-
-        const allLi = document.querySelectorAll('.uk-breadcrumb li');
-        const lastLi = allLi[allLi.length - 1];
-
-        lastLi.innerHTML = `<span>${breadChild}</span>`;
-    };
+            createLi.innerHTML = `<a href="${el.children[0].attributes[0].textContent}">${breadTitle}</a>`; 
+            breadWrapper[0].insertBefore(createLi, breadWrapper[0].firstElementChild.previousSibling);
+        })
+        
+        breadWrapper[0].lastChild.innerHTML = `<span>${breadTitle}</span>`;
+    }
 })();
